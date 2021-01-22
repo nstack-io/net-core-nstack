49dc076799e787828646d487e0d31d260f2e0d1a
Morten Turn Pedersen <motp@nodes.dk>
2021-01-22 10:42:16 +0100

    Implemented get all terms for language

--- NStack/NStack/Services/Implementation/NStackTermsService.cs
+++ NStack/NStack/Services/Implementation/NStackTermsService.cs
@@ -2,6 +2,7 @@
 using NStack.SDK.Repositories;
 using RestSharp;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace NStack.SDK.Services.Implementation
@@ -13,6 +14,17 @@ namespace NStack.SDK.Services.Implementation
         public NStackTermsService(INStackRepository repository)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public async Task<DataWrapper<IEnumerable<TermsEntry>>> GetAllTerms(string language)
+        {
+            if (language == null)
+                throw new ArgumentNullException(nameof(language));
+
+            var request = new RestRequest("api/v2/content/terms", Method.GET);
+            request.AddHeader("Accept-Language", language);
+
+            return await _repository.DoRequest<DataWrapper<IEnumerable<TermsEntry>>>(request);
         }
 
         public async Task<DataWrapper<Terms>> GetNewestTerms(string termsId, string userId, string language)
