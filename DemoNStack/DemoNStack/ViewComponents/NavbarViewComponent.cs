11887afafc1df2dbd299c48200e5732bb3b6173c
Morten Turn Pedersen <morten.turn-pedersen@monstar-lab.com>
2021-03-16 11:21:11 +0100

    Prepared version 1.3.1

--- DemoNStack/DemoNStack/ViewComponents/NavbarViewComponent.cs
+++ DemoNStack/DemoNStack/ViewComponents/NavbarViewComponent.cs
@@ -23,7 +23,7 @@ namespace DemoNStack.ViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var res = await NStackAppService.GetResourceAsync<Translation>(Request.GetCurrentLanguage(), NStackPlatform.Web, "1.3.0");
+            var res = await NStackAppService.GetResourceAsync<Translation>(Request.GetCurrentLanguage(), NStackPlatform.Web, "1.3.1");
 
             var viewModel = new NavbarViewModel
             {
